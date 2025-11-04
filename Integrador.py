0c39a77f5b7994b3dae9ea1d2f1cc22223d2692b
ortizluciiant <ortizluciiant@gmail.com>
2025-11-03 23:54:09 -0300

    Subiendo ajustes

--- Integrador.py
+++ Integrador.py
@@ -50,3 +50,48 @@ def guardar_paises(paises):
         for p in paises:
             linea= f"{p['nombre']},{p['poblacion']},{p['superficie']},{p['continente']}\n"
             f.write(linea)
+
+#AYE:
+
+# def agregar_paises
+# def actualizar_pais
+# def buscar_pais
+
+def mostrar_lista(paises):
+    
+    for p in paises:
+        print(f"{p['nombre']} - {p['poblacion']} hab - {p['superficie']} km² - {p['continente']}")
+
+def obtener_nombre(p):
+    return p["nombre"]
+
+def obtener_poblacion(p):
+    return p["poblacion"]
+
+def obtener_superficie(p):
+    return p["superficie"]
+
+def ordenar_por_nombre(paises):
+    paises.sort(key=obtener_nombre)
+    mostrar_lista(paises)
+
+
+def ordenar_por_poblacion(paises):
+    paises.sort(key=obtener_poblacion)
+    mostrar_lista(paises)
+
+
+def ordenar_por_superficie(paises, descendente=False):
+    paises.sort(key=obtener_superficie, reverse=descendente)
+    mostrar_lista(paises)
+
+#AYE:
+# def mostrar_estadisticas
+
+#LUCIA 
+# def menu
+
+#AYE llamada de funciones en bucle while
+
+#######finalizado#########
+
